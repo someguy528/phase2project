faaef38b7818fcbbf3cf53be73d4c6296eea2d02
Brandon Castro <castroyou999@gmail.com>
2023-01-15 15:44:49 -0500

    moved articles state and useEffect back into app.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,48 @@ import Home from './Home';
 import News from "./News";
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+    const [newsLoaded, setNewsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://phase-2-project-db.onrender.com/articles")
+            .then(resp => resp.json())
+            .then(data => {
+                setArticles(data);
+                setNewsLoaded(true);
+                console.log("loaded")
+            })
+    }, []);
+
+    function handleAddArticle(newArticle) {
+        setArticles([...articles, newArticle]);
+    };
+
+    function handleEditArticle(changedArticle) {
+        const updatedArticles = articles.map(article => {
+            if (article.id === changedArticle.id) {
+                return changedArticle
+            }
+            return article
+        });
+        setArticles(updatedArticles)
+    };
+
+    function handleRemoveArticle(removedArticle) {
+        const updatedArticles = articles.filter(article => article.id !== removedArticle.id);
+        setArticles(updatedArticles);
+    };
+
+    if (!newsLoaded) return <h1>Loading</h1>;
+
   
   return (
     <div>
       <NavBar />
       <Switch>
         <Route path="/news" >
-          <News />
+          <News articles={articles} onAddArticle={handleAddArticle} onEditArticle={handleEditArticle} onRemoveArticle={handleRemoveArticle} />
         </Route>
         <Route exact path='/'>
           <Home />
--- src/News.js
+++ src/News.js
@@ -6,42 +6,10 @@ import NewsList from "./NewsList";
 import NewsArticleEdit from "./NewsArticleEdit";
 import NewsAddArticle from "./NewsAddArticle";
 
-function News() {
+function News({articles, onAddArticle, onEditArticle, onRemoveArticle}) {
 
     const route = useRouteMatch().url;
-    const [articles, setArticles] = useState([]);
-    const [newsLoaded, setNewsLoaded] = useState(false);
-
-    useEffect(() => {
-        fetch("https://phase-2-project-db.onrender.com/articles")
-            .then(resp => resp.json())
-            .then(data => {
-                setArticles(data);
-                setNewsLoaded(true);
-            })
-    }, []);
-
-    function handleAddArticle(newArticle) {
-        setArticles([...articles, newArticle]);
-    };
-
-    function handleEditArticle(changedArticle) {
-        const updatedArticles = articles.map(article => {
-            if (article.id === changedArticle.id) {
-                return changedArticle
-            }
-            return article
-        });
-        setArticles(updatedArticles)
-    };
-
-    function handleRemoveArticle(removedArticle) {
-        const updatedArticles = articles.filter(article => article.id !== removedArticle.id);
-        setArticles(updatedArticles);
-    };
-
-    if (!newsLoaded) return <h1>Loading</h1>;
-
+    
 
     return (
         <div>
@@ -52,13 +20,13 @@ function News() {
                         <NewsList articles={articles} />
                     </Route>
                     <Route exact path={`${route}/addarticle`} >
-                        <NewsAddArticle onAddArticle={handleAddArticle} />
+                        <NewsAddArticle onAddArticle={onAddArticle} />
                     </Route>
                     <Route exact path={`${route}/:articleId`} >
-                        <NewsArticle articles={articles} onRemoveArticle={handleRemoveArticle} />
+                        <NewsArticle articles={articles} onRemoveArticle={onRemoveArticle} />
                     </Route>
                     <Route exact path={`${route}/:articleId/edit`} >
-                        <NewsArticleEdit articles={articles} onEditArticle={handleEditArticle} />
+                        <NewsArticleEdit articles={articles} onEditArticle={onEditArticle} />
                     </Route>
                 </Switch>
             </section>
